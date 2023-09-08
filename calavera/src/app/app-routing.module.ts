bbf6df347a93b848e5ffa9444c8be80384c36302
cap coldman <moctopus@outlook.com>
2023-09-08 09:45:57 -0400

    Adding auth0

--- calavera/src/app/app-routing.module.ts
+++ calavera/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { TriviaComponent } from './features/trivia/trivia.component';
 import { AppComponent } from './app.component';
 import { TriviaEditComponent } from './features/trivia/trivia-edit.component';
@@ -7,6 +8,7 @@ import { TriviaEditComponent } from './features/trivia/trivia-edit.component';
 const routes: Routes = [
   { 
     path: 'trivia', 
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'edit/:id',
